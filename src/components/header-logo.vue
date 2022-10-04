7f7b8a807f56798be0f32a0209af91da17f46d78
Fredrick Kisingo <acsdeveloper@africacloudspace.com>
2022-10-04 07:17:18 +0300

    changed router link to the main landing

--- src/components/header-logo.vue
+++ src/components/header-logo.vue
@@ -1,7 +1,7 @@
  <template>
 <!-- Logo -->
 <div class="header-left">
-<router-link to="/index" class="logo">
+<router-link to="/" class="logo">
 <img src="../assets/img/logo.png" width="40" height="40" alt="">
 </router-link>
 </div>
